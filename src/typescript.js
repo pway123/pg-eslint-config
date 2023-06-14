f594c8e5bca1198238502df4500f7521cc6d7a90
ShaoWei Teo <tookunokaze@gmail.com>
2023-06-14 16:12:48 +0800

    Remove usage of eslint-recommended (#4)
    With reference to https://typescript-eslint.io/linting/configs#eslint-recommended.
    
    The config `eslint-recommended` is used to modify `eslint:recommended` to support TypeScript better.
    
    As indicated in https://typescript-eslint.io/linting/configs#eslint-recommended, it is included in the other recommended configurations, and so we can follow the advice not to use it directly.

--- src/typescript.js
+++ src/typescript.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
